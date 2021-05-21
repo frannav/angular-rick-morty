25d78312d64c206ba632c917bc05fe8116e2443d
frannav <frannavmart@gmail.com>
2021-05-21 12:22:01 +0100

    Added Service Get Method with subscribe

--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -7,8 +7,17 @@ import { Injectable } from '@angular/core';
 export class CharactersService {
 
   private static apiBaseUrl = 'https://rickandmortyapi.com/api/character/';
+  private static apiId = '1';
+
 
   constructor(private http: HttpClient ) {
+  }
 
+  getCharacters() {
+    this.http.get(`${CharactersService.apiBaseUrl}/${CharactersService.apiId}`).subscribe(
+      (data) => {
+        console.log(data);
+      }
+    )
   }
 }
