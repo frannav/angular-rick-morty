e87e8b6759acd0fd264d8610909f6e9e998b7fe4
frannav <frannavmart@gmail.com>
2021-05-21 15:14:55 +0100

    Peta mas que una escopeta de feria

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CharactersService } from '../services/characters.service';
 
 @Component({
   selector: 'app-characters',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersComponent implements OnInit {
 
-  constructor() { }
+  public listOfCharacters: any[] = [];
+
+  constructor(public characterService: CharactersService) { }
 
   ngOnInit(): void {
+    this.loadCharacters();
+  }
+
+  loadCharacters() {
+    this.characterService.getAll().then(
+      (items) => {
+        this.listOfCharacters = items;
+        console.log(this.listOfCharacters);
+      }
+    ).catch(
+      (errr) => {
+        console.log(errr);
+      }
+    )
   }
 
 }
--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersService {
 
-  private static apiBaseUrl = 'https://rickandmortyapi.com/api/character/';
+  private static apiBaseUrl = 'https://rickandmortyapi.com/api/character/1,2,3,4';
 
 
   constructor(private http: HttpClient ) {
   }
 
-  
+  getAll(): Promise<any> {
+    return this.http.get('https://rickandmortyapi.com/api/character/1,2,3,4').pipe(take(1)).toPromise();
+  }
+
 }
