e404747d46032f8e5793d59cf2b0c3dc5dd775e4
frannav <frannavmart@gmail.com>
2021-05-21 10:23:15 +0100

    First commit 🚀

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './characters/characters.component';
+import { CharactersDetailComponent } from './characters-detail/characters-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/characters',
+    pathMatch: 'full',
+  },
+  {
+    path: 'characters',
+    component: CharactersComponent,
+  },
+  {
+    path: 'characters/:id',
+    component: CharactersDetailComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { CharactersDetailComponent } from './characters-detail/characters-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    CharactersDetailComponent
   ],
   imports: [
     BrowserModule,
