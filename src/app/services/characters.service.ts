e6e99b06155ac8b9c2e2f048a77ac101b4f090b8
frannav <frannavmart@gmail.com>
2021-05-21 12:16:50 +0100

    Delete component method

--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -8,18 +8,7 @@ export class CharactersService {
 
   private static apiBaseUrl = 'https://rickandmortyapi.com/api/character/';
 
-  constructor( private http: HttpClient ) {
+  constructor(private http: HttpClient ) {
 
-  }
-
-  get(characterId: string) {
-    try {
-      this.http.get(`${CharactersService.apiBaseUrl}/${characterId}`, {
-        
-      }).toPromise();
-    }
-    catch (error) {
-      console.log({ error });
-    }
   }
 }
